7446950251c2f3f793175ebf7ca9f48f671c8011
YeonjuLim <cjswn10@gmail.com>
2023-01-17 19:43:19 +0900

    api URL 수정

--- src/components/SelectOptions.vue
+++ src/components/SelectOptions.vue
@@ -25,7 +25,6 @@ export default {
   },
   created() {
     this.fetchOptions('http://18.183.198.182:3333/api/621f2588d8d85b8d78eb3e64')
-    // this.fetchOptions('http://18.183.198.182:3333/api/62419aa64139ba24abb709e8')
   },
   methods: {
     async fetchOptions(apiUrl) {
